f16669a44609c79ba5b17cd42cef1313c447e2eb
BillyanaL <115019424+BillyanaL@users.noreply.github.com>
2025-04-23 13:22:51 +0300

    first commit

---
+++ src/main/java/com/example/agrodirect/AgroDirectApplication.java
@@ -0,0 +1,13 @@
+package com.example.agrodirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroDirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroDirectApplication.class, args);
+    }
+
+}
